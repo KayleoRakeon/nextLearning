7547cfb3f2cf926590b289276e154d76e722de77
Benjamin Bourgouin <benjamin.bourgouin.pro@gmail.com>
2023-06-11 16:14:10 -0400

    Ajout du fichier _document.js

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+/** @format */
+
+// Librairies
+import Document, {
+   Html,
+   Head,
+   Main,
+   NextScript,
+} from 'next/document';
+
+export default function MonDocument() {
+   return (
+      <Html lang="fr">
+         <Head />
+         <body>
+            <Main />
+            <NextScript />
+         </body>
+      </Html>
+   );
+}
