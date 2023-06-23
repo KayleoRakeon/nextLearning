e947237491b35761cbeae38452174111f1e847ae
Benjamin Bourgouin <benjamin.bourgouin.pro@gmail.com>
2023-06-22 22:54:06 -0400

    creation du build

--- next.config.js
+++ next.config.js
@@ -11,6 +11,14 @@ const nextConfig = (phase) => {
             mongodb_username: 'ben-next',
             mongodb_password: 'yOLrmINVSBIuSYuK',
             mongodb_db: 'portfolio',
+
+            NEXTAUTH_URL: 'http://localhost:3000',
+            NEXT_URL: 'http://localhost:3000',
+            SECRET: '',
+            NEXTAUTH_SECRET:
+               'tZG2zn1hO3LloMfK7xbWjS6xnYHPR+qn98yy8g5R+Zg=',
+            NEXT_PUBLIC_SECRET: '',
+            JWT_SECRET: '',
          },
       };
    }
@@ -21,6 +29,14 @@ const nextConfig = (phase) => {
          mongodb_username: 'ben-next',
          mongodb_password: 'yOLrmINVSBIuSYuK',
          mongodb_db: 'portfolio',
+
+         NEXTAUTH_URL: 'http://localhost:3000',
+         NEXT_URL: 'http://localhost:3000',
+         SECRET: '',
+         NEXTAUTH_SECRET:
+            'tZG2zn1hO3LloMfK7xbWjS6xnYHPR+qn98yy8g5R+Zg=',
+         NEXT_PUBLIC_SECRET: '',
+         JWT_SECRET: '',
       },
    };
 };
